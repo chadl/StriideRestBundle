b97cc3576abca534fce14c2f9b71d84d29e075ae
Chad Lindstrom <chad.lindstrom@gmail.com>
2012-12-31 21:20:36 +0000

    adding download capability

--- Service/RestService.php
+++ Service/RestService.php
@@ -10,6 +10,28 @@ class RestService
   {
     $this->logger = $logger;
   }
+
+  public function download($url,$user_agent = null)
+  {
+    $filename = dirname(__FILE__) . "/file.download";
+
+    $this->logger->info(sprintf("RestService->get(%s)", $url));
+    $timeout = 0; // set to zero for no timeout
+
+    set_time_limit(0);
+    $fp = fopen($filename, 'w+');//This is the file where we save the    information
+    $ch = curl_init(str_replace(" ","%20",$url));//Here is the file we are downloading, replace spaces with %20
+    curl_setopt($ch, CURLOPT_TIMEOUT, 50);
+    curl_setopt($ch, CURLOPT_FILE, $fp); // here it sais to curl to just save it
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+    $data = curl_exec($ch);//get curl response
+    curl_close($ch);
+    fwrite($fp, $data);//write curl response to file
+    fclose($fp);
+    return $filename;
+  }
+
   public function get($url, $user_agent = null)
   {
     $this->logger->info(sprintf("RestService->get(%s)", $url));
